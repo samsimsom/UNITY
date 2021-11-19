3e07612a2f704c5b39191b461f7633a6e985fee9
sam sim som <samsimsom@gmail.com>
2021-11-19 09:34:52 +0300

    up

--- 2021/tool-dev-p1-01/Assets/Editor/BarrelTypeEditor.cs
+++ 2021/tool-dev-p1-01/Assets/Editor/BarrelTypeEditor.cs
@@ -4,55 +4,21 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
+// NOTES
+// explicit positioning using Rect
+// GUI
+// EditorGUI
+        
+// implicit positioning, auto-layout
+// GUILayout
+// EditorGUILayout
 
 [CustomEditor(typeof(BarrelType))]
 public class BarrelTypeEditor : Editor
 {
-    
-    public enum MyEnum
-    {
-        Bleep, Bloop, Blap
-    }
-
-    private MyEnum _myEnum;
-    
     public override void OnInspectorGUI()
     {
-        // explicit positioning using Rect
-        // GUI
-        // EditorGUI
-        
-        // implicit positioning, auto-layout
-        // GUILayout
-        // EditorGUILayout
-        
         base.OnInspectorGUI();
-        
-        DrawUILine(Color.black);
-        
-        GUILayout.Label("New UI", EditorStyles.boldLabel);
-        if (GUILayout.Button("Do Something!"))
-        {
-            Debug.Log($"Do IT! {_myEnum}");
-        }
-        
-        _myEnum = (MyEnum)EditorGUILayout.EnumPopup(_myEnum);
-
     }
-    
-    
-    public static void DrawUILine(Color color, 
-        int thickness = 2, int padding = 10)
-    {
-        Rect r = EditorGUILayout.GetControlRect(
-            GUILayout.Height(padding+thickness));
-        r.height = thickness;
-        r.y+=padding/2;
-        r.x-=2;
-        r.width +=6;
-        EditorGUI.DrawRect(r, color);
-    }
-    
-
 }
 
