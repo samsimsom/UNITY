952fda62dd01f2495d9aca9fbcb1c2bbf3357712
sam sim som <samsimsom@gmail.com>
2021-12-05 15:44:05 +0300

    up

--- 2021/tool-dev-p1-01/Assets/Editor/BarrelTypeEditor.cs
+++ 2021/tool-dev-p1-01/Assets/Editor/BarrelTypeEditor.cs
@@ -13,12 +13,39 @@ using UnityEngine;
 // GUILayout
 // EditorGUILayout
 
+[CanEditMultipleObjects]
 [CustomEditor(typeof(BarrelType))]
 public class BarrelTypeEditor : Editor
 {
+    private SerializedObject so;
+    private SerializedProperty propRadius;
+    private SerializedProperty propDamege;
+    private SerializedProperty propColor;
+
+
+    private void OnEnable()
+    {
+        so = serializedObject;
+        propRadius = so.FindProperty("radius");
+        propDamege = so.FindProperty("damage");
+        propColor = so.FindProperty("color");
+    }
+
+    
     public override void OnInspectorGUI()
     {
-        base.OnInspectorGUI();
+        so.Update();
+
+        EditorGUILayout.PropertyField(propRadius);
+        EditorGUILayout.PropertyField(propDamege);
+        EditorGUILayout.PropertyField(propColor);
+
+        so.ApplyModifiedProperties();
+        
+        // BarrelType barrel = target as BarrelType;
+        // barrel.radius = EditorGUILayout.FloatField("radius", barrel.radius);
+        // barrel.damage = EditorGUILayout.FloatField("damage", barrel.damage);
+        // barrel.color = EditorGUILayout.ColorField("color", barrel.color);
     }
 }
 
